bd15c0314a626bcf81b9f2da4b2076cc8e5c9823
rakeLL <satbirdd@gmail.com>
2013-09-25 17:24:48 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'f0f845aa7db7d111e7970843e8965efb8755f1073c0a73cebf74996d25be597dd938a53ebc7bea7ad982eddcd817d18370b3e7c5aa79253d36ef0e20fe58032a'
